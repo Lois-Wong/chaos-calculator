50f3602f4fba24429da05f034e5a89984816a69c
Lois Wong <loiswong@Loiss-Laptop.local>
2026-06-26 19:48:27 -0500

    create data.py

---
+++ data.py
@@ -0,0 +1,17 @@
+# alcohol_contentt : abv
+# alcohol_grams : grams of alcohol in the drink
+# sugar : grams 
+# caffeine : mg 
+
+# each key is 1 drink 
+
+drinks = [
+    {"name": "beer", "alcohol_content": 5.0, "alcohol_grams": 14.0, "sugar": 0.0, "caffeine": 0.0},
+    {"name": "red_wine", "alcohol_content": 13.0, "alcohol_grams": 19.5,"sugar": 1.0, "caffeine": 0.0},
+    {"name": "white_wine", "alcohol_content": 11.0, "alcohol_grams": 16.5, "sugar": 1.4, "caffeine": 0.0},
+    {"name": "gin", "alcohol_content": 46.0, "alcohol_grams": 20.24, "sugar": 0.0, "caffeine": 0.0},
+    {"name": "vodka", "alcohol_content": 40.0, "alcohol_grams": 17.6, "sugar": 0.0, "caffeine": 0.0},
+    {"name": "hugo_spritz", "alcohol_content": 9.0, "alcohol_grams": 12.5, "sugar": 11.0, "caffeine": 0.0},
+    {"name": "aperol_spritz", "alcohol_content": 9.0, "alcohol_grams": 12.5, "sugar": 13.0, "caffeine": 0.0},
+    {"name": "espresso", "alcohol_content": 0.0, "alcohol_grams": 0.0, "sugar": 0.0, "caffeine": 64.0},  
+] 
